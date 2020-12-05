116be6585994e2cfbf2c73e27af7016055db6d09
Ria Jayakar <71960598+RiaJayakar13@users.noreply.github.com>
2020-12-05 12:47:33 +0530

    Add files via upload

---
+++ Pig.js
@@ -0,0 +1,23 @@
+class Pig extends BaseClass {
+  constructor(x, y){
+    super(x,y,50,50);
+    this.image = loadImage("sprites/enemy.png");
+
+this.Visiblity=255;
+  }
+display(){
+
+console.log(this.body.speed)
+if(this.body.speed<3){
+super.display();
+}
+else {
+World.remove(world,this.body);
+push();
+this.Visiblity=this.Visiblity-20;
+tint(255,this.Visiblity);
+image(this.image,this.body.position.x,this.position.y,30,30);
+pop();
+}
+}
+}
